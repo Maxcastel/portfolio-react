f08ae50fdb8fd7f98aa2d5af8e473e41fe02e5e2
Maxence Castel <maxence.nord@gmail.com>
2024-04-17 23:00:25 +0200

    editor: add FixedToolbar + FloatingToolbar

--- src/components/admin/ProjectEditor.tsx
+++ src/components/admin/ProjectEditor.tsx
@@ -1,5 +1,12 @@
 import { Plate } from '@udecode/plate-common';
 import { Editor } from '@/components/plate-ui/editor';
+import { useRef } from 'react';
+import { cn } from '@udecode/cn';
+import { FixedToolbar } from '../plate-ui/fixed-toolbar';
+import { FixedToolbarButtons } from '../plate-ui/fixed-toolbar-buttons';
+import { TooltipProvider } from '../plate-ui/tooltip';
+import { FloatingToolbar } from '../plate-ui/floating-toolbar';
+import { FloatingToolbarButtons } from '../plate-ui/floating-toolbar-buttons';
 
 const initialValue = [
     {
@@ -13,15 +20,43 @@ const initialValue = [
 ];
 
 export function ProjectEditor(){
+    const containerRef = useRef(null);
 
     return (
-        <Plate 
-            initialValue={initialValue}
-            onChange={(newValue) => {
-                
-            }}
-        >
-            <Editor />
-        </Plate>
+        <TooltipProvider>
+            <div className="relative">
+                <Plate 
+                    initialValue={initialValue}
+                    onChange={(newValue) => {
+                        
+                    }}
+                >
+                    <div
+                        ref={containerRef}
+                        className={cn(
+                            'relative',
+                            // Block selection
+                            '[&_.slate-start-area-left]:!w-[64px] [&_.slate-start-area-right]:!w-[64px] [&_.slate-start-area-top]:!h-4'
+                        )}
+                    >
+                        <FixedToolbar>
+                            <FixedToolbarButtons />
+                        </FixedToolbar>
+
+                        <Editor
+                            className="px-[50px] py-16"
+                            autoFocus
+                            focusRing={false}
+                            variant="outline"
+                            size="md"
+                        />
+
+                        <FloatingToolbar>
+                            <FloatingToolbarButtons />
+                        </FloatingToolbar>
+                    </div>
+                </Plate>
+            </div>
+        </TooltipProvider>
     )
 }
