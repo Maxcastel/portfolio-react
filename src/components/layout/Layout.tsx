09b1aeeea62c10a0489796a70a1473694d8b4656
Maxence Castel <maxence.nord@gmail.com>
2024-04-22 11:50:12 +0200

    Layout: change max-w-7xl mx-auto to container

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,8 +4,8 @@ import { Header } from "./Header";
 export function Layout(){
 
     return (
-        <div className="min-h-full px-8">
-            <div className="max-w-7xl mx-auto">
+        <div className="min-h-full">
+            <div className="container">
                 <Header />
                 <Outlet />
             </div>
