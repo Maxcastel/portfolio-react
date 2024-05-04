303ce6dcfaabbbf50c413a5476fa64355f494a4e
Maxence Castel <maxence.nord@gmail.com>
2024-05-04 17:00:41 +0200

    add types data

---
+++ src/data/types.ts
@@ -0,0 +1,14 @@
+export const typeList = [
+    {
+      value: "scolaire",
+      label: "Scolaire",
+    },
+    {
+      value: "personnel",
+      label: "Personnel",
+    },
+    {
+      value: "professionnel",
+      label: "Professionnel",
+    },
+];
