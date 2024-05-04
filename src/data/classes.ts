c51caab5df6725bb7000670754f28c673df15aab
Maxence Castel <maxence.nord@gmail.com>
2024-05-04 17:01:24 +0200

    add classes data

---
+++ src/data/classes.ts
@@ -0,0 +1,14 @@
+export const classeList = [
+    {
+      value: "SIO 1",
+      label: "SIO 1",
+    },
+    {
+      value: "SIO 2",
+      label: "SIO 2",
+    },
+    {
+      value: "BUT 3",
+      label: "BUT 3",
+    },
+];
