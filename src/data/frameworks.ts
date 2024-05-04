54d4b8718eb16ec6f7e345fd2eea943fc28323e0
Maxence Castel <maxence.nord@gmail.com>
2024-05-04 16:59:25 +0200

    add frameworks data

---
+++ src/data/frameworks.ts
@@ -0,0 +1,18 @@
+export const frameworksList = [
+    {
+      value: "symfony",
+      label: "Symfony",
+    },
+    {
+      value: "nodejs",
+      label: "NodeJS",
+    },
+    {
+      value: "expressjs",
+      label: "ExpressJS",
+    },
+    {
+      value: "react",
+      label: "React",
+    },
+];
