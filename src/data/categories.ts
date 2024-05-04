2f7d8cf8c02dfa5c0a73e5db1435c6bfc8e04343
Maxence Castel <maxence.nord@gmail.com>
2024-05-04 17:00:58 +0200

    add categories data

---
+++ src/data/categories.ts
@@ -0,0 +1,14 @@
+export const categoryList = [
+    {
+      value: "web",
+      label: "Web",
+    },
+    {
+      value: "mobile",
+      label: "Mobile",
+    },
+    {
+      value: "logiciel",
+      label: "Logiciel",
+    },
+];
