b9f279c3745c79665f5e8ada15be688a9d3b8a0f
Maxence Castel <maxence.nord@gmail.com>
2024-05-04 16:59:40 +0200

    add languages data

---
+++ src/data/languages.ts
@@ -0,0 +1,18 @@
+export const languagesList = [
+    {
+      value: "php",
+      label: "PHP",
+    },
+    {
+      value: "javascript",
+      label: "Javascript",
+    },
+    {
+      value: "typescript",
+      label: "Typescript",
+    },
+    {
+      value: "java",
+      label: "Java",
+    },
+];
