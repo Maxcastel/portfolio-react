c3cdccff9ce969165ac68a6ff539f96a9e907238
Maxcastel <maxence.nord@gmail.com>
2024-06-16 01:11:49 +0200

    add Emails

---
+++ src/components/admin/email/Emails.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+import { Email } from "./Email";
+import { EmailCard } from "./EmailCard";
+
+export function Emails(){
+    const [emails, setEmails] = useState<Email[]>([]);
+
+    useEffect(() => {
+        fetch("/api/emails")
+        .then((res) => res.json())
+        .then((emails) => {
+          console.log("emails",emails.data);
+          setEmails(emails.data);
+        })
+    }, [])
+
+    return (
+        <>
+            <h1 className="text-2xl font-bold my-4">
+                Emails
+            </h1>
+            
+            <div className="flex flex-col items-start gap-y-4">
+                {emails.slice().reverse().map((email) => (
+                    <EmailCard key={email.id} 
+                        email={email} 
+                    />
+                ))}
+            </div>
+        </>
+    )
+}
