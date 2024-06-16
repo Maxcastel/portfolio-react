cd425b2a93d0771c174109543e768d7bdc38b3bf
Maxcastel <maxence.nord@gmail.com>
2024-06-16 17:23:26 +0200

    add useContactEditor

---
+++ src/hooks/useContactEditor.ts
@@ -0,0 +1,108 @@
+import { TDescendant, TElement, Value } from "@udecode/plate-common";
+import { CSSProperties, createElement, useEffect, useState } from "react";
+
+interface Style{
+    color?: string;
+    backgroundColor?: string;
+}
+
+type TextAlign = "center" | "left" | "right";
+
+export function useContactEditor(initialValue?:Value){
+    const [contentJson, setContentJson] = useState<Value|undefined>(initialValue);
+    const [contentHtml, setContentHtml] = useState<any>();
+
+    useEffect(() => {
+        let element;
+        if (contentJson){
+            element = createElement("div", null, contentJson.map((element:TElement) => {
+                let style:CSSProperties = {};
+                switch (element.type){
+                    case "p":
+                            style = {...style, fontSize: "1.125rem"}
+                            break;
+                        case "h1":
+                            style = {...style, fontSize: "2.25rem", fontWeight: 700, marginBottom: "0.25rem"}
+                            break;
+                        case "h2":
+                            style = {...style, fontSize: "1.5rem", fontWeight: 600, marginTop: "1.4em"}
+                            break;
+                        case "h3":
+                            style = {...style, fontSize: "1.25rem", fontWeight: 600}
+                            break;
+                        case "h4":
+                            style = {...style, fontSize: "1.125rem", fontWeight: 600}
+                            break;
+                        case "h5":
+                            style = {...style, fontSize: "1.125rem", fontWeight: 600}
+                            break;
+                        case "h6":
+                            style = {...style, fontSize: "1rem", fontWeight: 600}
+                            break;
+                }
+                const firstChildText = element.children[0].text as string;
+
+                if(firstChildText.trim() === ""){
+                    style = {...style, height: "28px"}
+                }
+                if (element.align){
+                    style = {...style, textAlign: element.align as TextAlign}
+                }
+                return (
+                    createElement(element.type, { style }, element.children.map((child:TDescendant) => {
+                        let tags = [];
+                        if (child.bold) tags.push("b")
+                        if (child.italic) tags.push("i")
+                        if (child.underline) tags.push("u")
+                        if (child.strikethrough) tags.push("s")
+                        let style:CSSProperties = {};
+                        if (child.color) style = {...style, color: child.color as string} 
+                        if (child.backgroundColor) style = {...style, backgroundColor: child.backgroundColor as string} 
+                        
+                        // if (tags.length === 0){
+                        //     return (
+                        //         createElement("span", { style }, child.text as string)
+                        //     )
+                        // }
+                        // else {
+                        //     let content:any = child.text as string;
+                        //     for (let i = tags.length - 1; i >= 0; i--) {
+                        //         const tag = tags[i];
+                        //         content = createElement(tag, { style }, content);
+                        //     }
+                        //     return createElement("span", { style }, content);
+                        // }
+
+                        return (
+                            tags.length === 0 ?
+                                createElement("span", { style }, child.text as string)
+                            : tags.reduceRight((acc:any, tag) => createElement(tag, { style }, acc), child.text as string)
+                        )
+
+                        // if (tags.length === 0) {
+                        //     return createElement("span", { style }, child.text as string);
+                        // } else {
+                        //     return tags.reduceRight((acc, tag, index) => {
+                        //         if (index === tags.length - 1) {
+                        //             return createElement(tag, { style }, acc);
+                        //         } else {
+                        //             return createElement(tag, null, acc);
+                        //         }
+                        //     }, child.text as string);
+                        // }
+                    }))
+                )
+
+                        
+            })) 
+        }
+
+        setContentHtml(element)
+    }, [contentJson])
+    
+    return [
+        JSON.stringify(contentJson), 
+        setContentJson,
+        contentHtml
+    ] as const
+}
