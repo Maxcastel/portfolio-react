4f8c85d1e6b5712f52759d1db74430009d40b84a
Maxcastel <maxence.nord@gmail.com>
2024-06-16 16:40:16 +0200

    ShowEmail: change text-center to flex-col items-center & add min width max width break-words

--- src/components/admin/email/ShowEmail.tsx
+++ src/components/admin/email/ShowEmail.tsx
@@ -23,7 +23,7 @@ export function ShowEmail(){
     if (loading) return
 
     return (
-        <div className="text-center my-4">
+        <div className="flex flex-col items-center my-4">
             {email && <>
                 <h1 className="text-xl font-semibold">
                     {t('contact.objet')} {email.subject}
@@ -33,7 +33,7 @@ export function ShowEmail(){
                 </h2>
                 <p className="text-muted-foreground">{new Date(email.sendDate).toLocaleString('fr-FR', { timeZone: 'UTC'})}</p>
 
-                <div className="mt-8" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(email.message) }} />
+                <div className="mt-8 break-words min-w-80 max-w-lg bg-red-50" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(email.message) }} />
             </>}
         </div>
     )
