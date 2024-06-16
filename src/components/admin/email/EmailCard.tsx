0b57c484318bcd45aa8425589267ebfa541880f4
Maxcastel <maxence.nord@gmail.com>
2024-06-16 02:38:42 +0200

    add EmailCard

---
+++ src/components/admin/email/EmailCard.tsx
@@ -0,0 +1,36 @@
+import { useTranslation } from "react-i18next";
+import { Email } from "./Email";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+
+export function EmailCard({email}:{email:Email}){
+    const { t } = useTranslation();
+    
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>{t('contact.objet')} {email.subject}</CardTitle>
+                <CardDescription>
+                    {t('contact.from')} {email.email} <br />
+                    {new Date(email.sendDate).toLocaleString('fr-FR', { timeZone: 'Europe/Paris'})}
+                </CardDescription>
+            </CardHeader>
+            <CardFooter>
+                <CardDescription>
+                    <Button asChild>
+                        <a href={`/admin/emails/${email.id}`}>
+                            {t('contact.seeBtn')}
+                        </a>
+                    </Button>
+                </CardDescription>
+            </CardFooter>
+        </Card>
+    )
+}
