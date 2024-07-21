ab6a73d691da8a6650977b30888d08f1f1bc9939
Maxcastel <maxence.nord@gmail.com>
2024-07-21 21:54:20 +0200

    Hero: 1.5rem to 2rem in second text clamp

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -13,7 +13,7 @@ export function Hero(){
                 <h1 className="text-[clamp(3rem,10vw,6rem)] font-extrabold bg-gradient-to-t from-slate-900 to-slate-500 bg-clip-text text-transparent dark:bg-gradient-to-t dark:from-slate-100 dark:to-slate-300">
                     Maxence Castel
                 </h1>
-                <h2 className="text-[clamp(1.5rem,10vw,3rem)] font-bold dark:bg-gradient-to-r dark:from-slate-50 dark:to-slate-500 dark:bg-clip-text dark:text-transparent">
+                <h2 className="text-[clamp(2rem,10vw,3rem)] font-bold dark:bg-gradient-to-r dark:from-slate-50 dark:to-slate-500 dark:bg-clip-text dark:text-transparent">
                     {t('developpeurfullstack')}
                 </h2>
             </div>
