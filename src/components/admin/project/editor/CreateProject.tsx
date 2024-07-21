3f4cfce0e445d3a460f79678075a0b89c1a0c3e7
Maxcastel <maxence.nord@gmail.com>
2024-07-21 21:12:54 +0200

    CreateProject: add token to fetch

--- src/components/admin/project/editor/CreateProject.tsx
+++ src/components/admin/project/editor/CreateProject.tsx
@@ -139,7 +139,8 @@ export function CreateProject(){
                 classe_name: data.classe.length === 0 ? null : data.classe[0],
             }),
             headers: {
-                'Content-type': 'application/json; charset=UTF-8'
+                'Content-type': 'application/json; charset=UTF-8',
+                'Authorization': `Bearer ${window.localStorage.getItem('token')}`
             }
         })
         .then(response => response.json())
