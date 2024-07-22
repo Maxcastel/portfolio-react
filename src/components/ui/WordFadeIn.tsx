cd2c1478c57e474d8f16ac607f5bb4da6f65b705
Maxcastel <maxence.nord@gmail.com>
2024-07-22 12:11:21 +0200

    WordFadeIn: remove md:leading-[5rem]

--- src/components/ui/WordFadeIn.tsx
+++ src/components/ui/WordFadeIn.tsx
@@ -30,7 +30,7 @@ export default function WordFadeIn({
       initial="hidden"
       animate="visible"
       className={cn(
-        "font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:leading-[5rem]",
+        "font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white",
         className,
       )}
     >
