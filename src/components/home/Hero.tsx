59ed02ecae86e5b1e59a6be93b81bca89ca5e71e
Maxcastel <maxence.nord@gmail.com>
2024-07-22 11:54:52 +0200

    Hero: add leading-tight

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -9,7 +9,7 @@ export function Hero(){
     return (
         <div className="px-4 flex items-center h-[calc(100vh-(24px+32px))] lg:h-[calc(100vh-(40px+32px))] pb-[calc(24px+32px)] lg:pb-[calc(40px+32px)]">
 
-            <div className="flex flex-col">
+            <div className="flex flex-col leading-tight">
                 <h1 className="text-[clamp(3rem,10vw,6rem)] font-extrabold bg-gradient-to-t from-slate-900 to-slate-500 bg-clip-text text-transparent dark:bg-gradient-to-t dark:from-slate-100 dark:to-slate-300">
                     Maxence Castel
                 </h1>
