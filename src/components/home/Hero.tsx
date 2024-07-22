e9625784048f968abf7791c68d5fa8b215e0f0b7
Maxcastel <maxence.nord@gmail.com>
2024-07-22 18:02:13 +0200

    Hero: change second text to FadeText

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { FadeText } from "../ui/FadeText";
 
 export function Hero(){
     const { t } = useTranslation();
@@ -13,9 +14,14 @@ export function Hero(){
                 <h1 className="text-[clamp(3rem,10vw,6rem)] font-extrabold bg-gradient-to-t from-slate-900 to-slate-500 bg-clip-text text-transparent dark:bg-gradient-to-t dark:from-slate-100 dark:to-slate-300">
                     Maxence Castel
                 </h1>
-                <h2 className="text-[clamp(2rem,10vw,3rem)] font-bold dark:bg-gradient-to-r dark:from-slate-50 dark:to-slate-500 dark:bg-clip-text dark:text-transparent">
-                    {t('developpeurfullstack')}
-                </h2>
+                <FadeText
+                    className="text-left tracking-normal text-[clamp(2rem,10vw,3rem)] font-bold dark:bg-gradient-to-r dark:from-slate-50 dark:to-slate-500 dark:bg-clip-text dark:text-transparent"
+                    direction="down"
+                    framerProps={{
+                        show: { transition: { delay: 0.2 } },
+                    }}
+                    text={t('developpeurfullstack')}
+                />
             </div>
             
         </div>
