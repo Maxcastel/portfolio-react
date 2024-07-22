6bd61b9b54156bf0a0ca7b1e38c9d051f31ad6c2
Maxcastel <maxence.nord@gmail.com>
2024-07-23 01:40:30 +0200

    HomePage: wrap components with RevealOnScroll

--- src/components/home/HomePage.tsx
+++ src/components/home/HomePage.tsx
@@ -4,17 +4,33 @@ import { Hero } from "./Hero";
 import { Projects } from "./project/Projects";
 import { Skills } from "./skill/Skills";
 import { Contact } from "./contact/Contact";
+import { RevealOnScroll } from "../RevealOnScroll";
 
 export function HomePage(){
 
     return (
         <>
             <Hero />
-            <Projects />
-            <Experience />
-            <Formations />
-            <Skills />
-            <Contact />
+
+            <RevealOnScroll>
+                <Projects />
+            </RevealOnScroll>
+
+            <RevealOnScroll>
+                <Experience />
+            </RevealOnScroll>
+
+            <RevealOnScroll>
+                <Formations />
+            </RevealOnScroll>
+
+            <RevealOnScroll>
+                <Skills />
+            </RevealOnScroll>
+
+            <RevealOnScroll>
+                <Contact />
+            </RevealOnScroll>
         </>
     )
 
